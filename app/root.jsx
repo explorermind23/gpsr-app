d15329bb12319f7717e334531aae842b9c961e17
explorermind23 <explorermind23@gmail.com>
2026-07-18 12:39:36 +0200

    fix root for embedded app

--- app/root.jsx
+++ app/root.jsx
@@ -1,10 +1,24 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
 export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="shopify-api-key" content="63226bc8c9449ef95ec553cef9682e6f" />
+        <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"></script>
+        <link rel="preconnect" href="https://cdn.shopify.com/" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css"
+        />
         <Meta />
         <Links />
       </head>
