95da5705700fcb58f7ce09f523fbfd00a3e47e0f
explorermind23 <explorermind23@gmail.com>
2026-07-18 12:23:43 +0200

    add auth and webhook routes

---
+++ app/routes/webhooks.jsx
@@ -0,0 +1,7 @@
+import { authenticate } from "../shopify.server";
+
+export const action = async ({ request }) => {
+  const { topic, shop } = await authenticate.webhook(request);
+  console.log(`Webhook: ${topic} for ${shop}`);
+  return new Response();
+};
